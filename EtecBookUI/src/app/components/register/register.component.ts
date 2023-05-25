04e75adad6005de723380e1966b6e6c5d4cd0325
Allana Cervati <allana.cervati@etec.sp.gov.br>
2023-05-25 20:38:08 -0300

    aula 25-05

--- EtecBookUI/src/app/components/register/register.component.ts
+++ EtecBookUI/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,62 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
-}
+  registerForm!: FormGroup;
+  
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void{
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required , Validators.email])],
+      password: ['',  Validators.compose([Validators.required , Validators.minLength(6)])],
+    });
+  }
+
+  get f (){
+    return this.registerForm.controls;
+  }
+
+  checkName(){
+    return this.f['name'].dirty && this.f['name'].errors?.['required'];
+  }
+
+  checkEmail(){
+    return this.f['email'].dirty && this.f['email'].errors?.['required'];
+  }
+
+  checkEmailValid(){
+    return this.f['email'].dirty && this.f['email'].errors?.['email'];
+  }
+
+  checkPassword() {
+    return this.f['password'].dirty && this.f['password'].errors?.['required'];
+  }
+
+  checkPasswordLenght() {
+    return this.f['password'].dirty && this.f['password'].errors?.['minLenght'];
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid){
+      //Enviar os dados ao backend
+      console.log(this.registerForm.value);
+    } else {
+      //Dispara um erro
+        this.ValidateAllFormFields(this.registerForm);
+      }
+    }
+  
+    private ValidateAllFormFields(formGroup: FormGroup) {
+      Object.keys(formGroup.controls).forEach(field => {
+        const control = formGroup.get(field);
+        if (control instanceof FormControl) {
+          control.markAsDirty({ onlySelf: true });
+         } else if(control instanceof FormGroup) {
+            this.ValidateAllFormFields(control);
+          }
+      });
+    }
+  
+  }
+  
